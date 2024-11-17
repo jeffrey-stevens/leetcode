fae0a1a7085cfe8d66b1a244a2237f6219f3c1fa
Jeffrey Stevens <47906337+jeffrey-stevens@users.noreply.github.com>
2024-11-16 22:57:14 -0500

    The test array and target data are now encapsulated in structs.  Test array and target data reporting were collected under a single function.

--- 001-two-sum/test.c
+++ 001-two-sum/test.c
@@ -9,95 +9,125 @@ const int NUMS_SIZE_MAX = 20;
 const int MAX_NUM = 50;
 
 
-void build_nums(int ** nums, int * numsSize) {
+typedef struct {
+    int * array;
+    int size;
+} NumsArray;
 
-    int array_size = NUMS_SIZE_MIN + rand() % (NUMS_SIZE_MAX - NUMS_SIZE_MIN + 1);
+typedef struct {
+    int i;
+    int j;
+    int num1;
+    int num2;
+    int sum;
+} Target;
+
+
+NumsArray build_nums() {
+
+    NumsArray nums; 
+
+    // Randomly select the array size
+    nums.size = NUMS_SIZE_MIN + rand() % (NUMS_SIZE_MAX - NUMS_SIZE_MIN + 1);
 
     // Create the array of integers
-    int * nums_array = (int *) malloc(array_size * sizeof(int));
-    if (nums_array == NULL) {
+    nums.array = (int *) malloc(nums.size * sizeof(int));
+    if (nums.array == NULL) {
         puts("Could not allocate memory for the test array.");
         exit(EXIT_FAILURE);
     }
 
     // Populate the array with random integers
-    for (int i = 0; i < array_size; ++i) {
-        nums_array[i] = rand() % (2 * MAX_NUM + 1) - MAX_NUM;
+    for (int i = 0; i < nums.size; ++i) {
+        nums.array[i] = rand() % (2 * MAX_NUM + 1) - MAX_NUM;
         // Should check that these aren't equal to any of the other numbers...
     }
 
-    // Print the array
-    printf("Input array of %d numbers: [", array_size);
-    for (int i = 0; i < array_size; ++i) {
-        printf("%d", nums_array[i]);
-        if (i < array_size - 1) {
-            printf(", ");
-        }
-    }
-    printf("]\n");
-
-    // Output the array and array size
-    *nums = nums_array;
-    *numsSize = array_size;
+    return nums;
 }
 
 
-int choose_target(int const nums[], int numsSize) {
+void free_nums(NumsArray * nums) {
+    free(nums->array);
+    nums->array = NULL;
+    nums->size = 0;
+}
+
+
+Target choose_target(const NumsArray nums) {
 
     // Choose two elements for the target
-    int i = rand() % numsSize;
-    int j;
-    do {
-        // The two indices should be different
-        j = rand() % numsSize;
-    } while (j == i);
+    int i = rand() % (nums.size - 1);
+    int j = (i + 1) + rand() % (nums.size - (i + 1));
 
-    int num1 = nums[i];
-    int num2 = nums[j];
-    int target = num1 + num2;
+    int num1 = nums.array[i];
+    int num2 = nums.array[j];
+    int sum = num1 + num2;
 
-    printf("The selected numbers are %d (index %d) and %d (index %d).\n", num1, i, num2, j);
-    printf("The target sum is %d.\n", target);
+    Target target = {
+        .i = i,
+        .j = j,
+        .num1 = num1,
+        .num2 = num2,
+        .sum = sum
+    };
 
     return target;
 }
 
 
+void print_sampling(const NumsArray nums, const Target target) {
+
+    // Print the array
+    printf("Input array of %d numbers: [", nums.size);
+    for (int i = 0; i < nums.size; ++i) {
+        printf("%d", nums.array[i]);
+        if (i < nums.size - 1) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
+
+    // Print the target
+    printf("The selected numbers are %d (index %d) and %d (index %d).\n",
+        target.num1, target.i, target.num2, target.j);
+    printf("The target sum is %d.\n", target.sum);
+
+    printf("\n");
+}
+
+
 void test_two_sum() {
-    int * nums;
-    int numsSize;
-    int target;
-    int returnSize;
-    int * indices;
 
     srand(RAND_SEED);
     
-    // Create a random array of integers
-    build_nums(&nums, &numsSize);
+    // Create a random array of integers and the target, and print the results
+    NumsArray nums = build_nums();
+    Target target = choose_target(nums);
+    print_sampling(nums, target);
 
-    // Choose the target
-    target = choose_target(nums, numsSize);
-
-    printf("\n");
-
-    indices = twoSum(nums, numsSize, target, &returnSize);
+    // Find two numbers that sum to the target
+    int returnSize;
+    int * indices = twoSum(nums.array, nums.size, target.sum, &returnSize);
 
     // Assumes that the size of the returned array is 2
     int i = indices[0];
     int j = indices[1];
-    int num1 = nums[i];
-    int num2 = nums[j];
+    int num1 = nums.array[i];
+    int num2 = nums.array[j];
+    int sum = num1 + num2;
     printf("The algorithm has selected numbers %d (index %d) and %d (index %d).\n",
         num1, i, num2, j);
-    printf("The sum of the two is %d.\n", num1 + num2);
+    printf("The sum of the two is %d.\n", sum);
 
     // Report the results of the test
-    if (num1 + num2 == target) {
+    if (sum == target.sum) {
         printf("The test was successful!\n");
     } else {
         printf("The test failed.\n");
     }
 
-    free(nums);
+    // Free all allocated memory
+    free_nums(&nums);
     free(indices);
 }
