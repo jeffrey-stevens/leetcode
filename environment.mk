930da46f67f0dd1d415f5bd7c02f01b3e579c78e
Jeffrey Stevens <47906337+jeffrey-stevens@users.noreply.github.com>
2024-11-18 20:52:39 -0500

    Now stores common compilation variables in a separate makefile.

--- environment.mk
+++ environment.mk
@@ -1,7 +1,19 @@
 # LeetCode environment
 
 CC = gcc
-CSTANDARD = -std=gnu11
-OPTIM = -O2
 
-CFLAGS = $(CSTANDARD) -Wall
+CSTANDARD = -std=gnu11
+WARNINGS_CFLAGS = -Wall -Wextra
+
+ifeq ($(DEBUG), 1)
+OPTIM = -O0
+DEBUG_CFLAGS = -g -DDEBUG
+
+else
+OPTIM = -O2
+DEBUG_CFLAGS = -DDEBUG
+
+endif
+
+
+CFLAGS = $(CSTANDARD) $(WARNINGS_CFLAGS) $(OPTIM) $(DEBUG_CFLAGS)
