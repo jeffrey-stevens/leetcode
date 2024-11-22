c6ebdd48a5c44b16dceabc8c37b68cd9f2fd46a5
Jeffrey Stevens <47906337+jeffrey-stevens@users.noreply.github.com>
2024-11-21 23:04:21 -0500

    Problem 001:  Created header file for command-line options handlers.

---
+++ 001/option-handlers.h
@@ -0,0 +1,12 @@
+#ifndef OPTION_HANDLERS_H
+#define OPTION_HANDLERS_H
+
+#include "options.h"
+
+void parse_seed(char * arg, Options * options);
+
+void parse_num_max(char * arg, Options * options);
+
+void parse_array_size_max(char * arg, Options * options);
+
+#endif
